478dc8cdc3760a24cff456982079d46a678ef903
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-09 14:57:07 -0300

    Adicionado fragmento de login na MainActivity e removida atividade de login

---
+++ app/src/main/java/com/hinade/fleetfix/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.hinade.fleetfix
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.hinade.fleetfix.databinding.FragmentLoginBinding
+
+/**
+ * Um fragmento representando a tela de login.
+ */
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+
+        with(binding) {
+            loginButton.setOnClickListener {
+                if (usuarioEditText.text.isBlank() || senhaEditText.text.isBlank()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Preencha todos os campos!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Login realizado com sucesso!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
+        return binding.root
+    }
+}
