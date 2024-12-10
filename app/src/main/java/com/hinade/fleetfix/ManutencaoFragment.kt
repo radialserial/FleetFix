4ec19198c253b225fe1063e0f0db9f8e58483f3a
Debora <deborarezende09@gmail.com>
2024-12-10 17:42:46 -0300

    Add tela de manutenção mecânico

---
+++ app/src/main/java/com/hinade/fleetfix/ManutencaoFragment.kt
@@ -0,0 +1,18 @@
+package com.hinade.fleetfix
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hinade.fleetfix.R
+
+class ManutencaoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manutencao, container, false)
+    }
+}
