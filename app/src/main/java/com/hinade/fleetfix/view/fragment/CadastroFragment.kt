0065f6975df62d356e2a11a2ec2e6ab5107d9fcb
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-10 15:36:14 -0300

    Adicionado back-end de cadastro

--- app/src/main/java/com/hinade/fleetfix/view/fragment/CadastroFragment.kt
+++ app/src/main/java/com/hinade/fleetfix/view/fragment/CadastroFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hinade.fleetfix.R
 import com.hinade.fleetfix.databinding.FragmentCadastroBinding
-import com.hinade.fleetfix.model.TipoUsuario
+import com.hinade.fleetfix.model.usuario.TipoUsuario
+import com.hinade.fleetfix.model.usuario.Usuario
+import com.hinade.fleetfix.viewmodel.CadastroViewModel
 
 /**
  * Um fragmento representando a tela de cadastro.
@@ -18,6 +21,7 @@ import com.hinade.fleetfix.model.TipoUsuario
 class CadastroFragment : Fragment() {
 
     private lateinit var binding: FragmentCadastroBinding
+    private val cadastroViewModel by viewModels<CadastroViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,26 +41,64 @@ class CadastroFragment : Fragment() {
             tipoUsuarioSpinner.adapter = adapter
 
             cadastroButton.setOnClickListener {
-                if (
-                    usuarioEditText.text.isBlank() ||
-                    senhaEditText.text.isBlank() ||
-                    repetirSenhaEditText.text.isBlank()
-                ) {
-                    notificar("Preencha todos os campos!")
-                } else if (senhaEditText.text.toString() != repetirSenhaEditText.text.toString()) {
-                    notificar("As senhas não coincidem!")
-                } else if (
-                    TipoUsuario.from(tipoUsuarioSpinner.selectedItem.toString()) == null
-                ) {
-                    notificar("Escolha um usuário.")
-                } else {
-                    notificar("Cadastro realizado com sucesso!")
-                    findNavController().navigate(R.id.action_cadastroFragment_to_loginFragment)
+
+                val nome = nomeEditText.text.toString()
+                val telefone = telefoneEditText.text.toString()
+                val login = loginEditText.text.toString()
+                val senha = senhaEditText.text.toString()
+                val repetirSenha = repetirSenhaEditText.text.toString()
+                val tipoUsuario = tipoUsuarioSpinner.selectedItem.toString()
+
+                if (camposEstaoValidos(login, senha, repetirSenha, tipoUsuario)) {
+                    try {
+                        cadastroViewModel.insertUsuario(
+                            Usuario(
+                                nome,
+                                login,
+                                senha,
+                                telefone,
+                                TipoUsuario.from(tipoUsuario)!!
+                            )
+                        )
+
+                        notificar("Cadastro realizado com sucesso!")
+                        findNavController().navigate(R.id.action_cadastroFragment_to_loginFragment)
+
+                    } catch (e: Exception) {
+                        notificarLongo("Erro: ${e.message}")
+                    }
                 }
             }
         }
 
         return binding.root
+    }
+
+    private fun camposEstaoValidos(
+        login: String,
+        senha: String,
+        repetirSenha: String,
+        tipoUsuario: String,
+    ): Boolean {
+
+        if (login.isBlank() || senha.isBlank() || repetirSenha.isBlank()) {
+            notificar("Preencha todos os campos!")
+            return false
+        }
+
+        if (senha != repetirSenha) {
+            notificar("As senhas não coincidem!")
+            return false
+        }
+
+        if (
+            TipoUsuario.from(tipoUsuario) == null
+        ) {
+            notificar("Escolha um usuário.")
+            return false
+        }
+
+        return true
     }
 
     private fun notificar(mensagem: String) {
@@ -66,4 +108,13 @@ class CadastroFragment : Fragment() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
+    private fun notificarLongo(mensagem: String) {
+        Toast.makeText(
+            requireContext(),
+            mensagem,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
 }
