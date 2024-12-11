ecc445685d3ae82275d245cc606a9811298c3c40
Debora <deborarezende09@gmail.com>
2024-12-10 21:18:57 -0300

    App tela de Serviços Fragment (Mecanico)

--- app/src/main/java/com/hinade/fleetfix/ServicosFragment.kt
+++ app/src/main/java/com/hinade/fleetfix/ServicosFragment.kt
@@ -1,10 +1,10 @@
 package com.hinade.fleetfix
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ServicosMFragment.newInstance] factory method to
+ * Use the [ServicosFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ServicosMFragment : Fragment() {
+class ServicosFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ServicosMFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_servicos_m, container, false)
+        return inflater.inflate(R.layout.fragment_servicos, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class ServicosMFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ServicosMFragment().apply {
+            ServicosFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
